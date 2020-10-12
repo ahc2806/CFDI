e59c299c95e6e63a65dd76543fe3f77ee8799129
ahc2806 <alonsohernandezcortez@gmail.com>
2020-10-12 03:34:49 -0500

    Multi conceptos y traslados

--- src/main/WriteXML.java
+++ src/main/WriteXML.java
@@ -24,6 +24,8 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -56,12 +58,15 @@ import org.apache.xerces.impl.dv.util.Base64;
 
 public class WriteXML {
     
+    Date date = new Date();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+    
     public void crearComprobante() throws Exception {
         XMLGregorianCalendar fecha = null;
         
         //Genera la fecha de emisión
         try{
-            fecha= DatatypeFactory.newInstance().newXMLGregorianCalendar("2020-10-11T14:00:00");
+            fecha= DatatypeFactory.newInstance().newXMLGregorianCalendar(sdf.format(date));
         } catch (DatatypeConfigurationException ex) {
             Logger.getLogger(WriteXML.class.getName()).log(Level.SEVERE,null, ex);
         }
@@ -77,14 +82,14 @@ public class WriteXML {
         xml.setFormaPago("01");
         xml.setCondicionesDePago("En una sóla exhibición");
         //Datos generales
-        xml.setSubTotal(new BigDecimal("500.00"));
+        xml.setSubTotal(new BigDecimal("1100.00"));
         //xml.setDescuento(new BigDecimal("0.00"));
         xml.setMoneda(CMoneda.MXN);
         xml.setTipoCambio(new BigDecimal("1"));
-        xml.setTotal(new BigDecimal("580.00"));
+        xml.setTotal(new BigDecimal("1276.00"));
         xml.setTipoDeComprobante(CTipoDeComprobante.I);
         xml.setMetodoPago(CMetodoPago.PUE);
-        xml.setLugarExpedicion("71200");
+        xml.setLugarExpedicion("94930");
         
         //Este bloque es para crear el emisor
         xml.setEmisor(createEmisor(of));
@@ -147,9 +152,7 @@ public class WriteXML {
         System.out.print("XML generado correctamente!");
     }
     
-    
-    //Metodos
-    
+    //Genera los datos del emisor
     private Emisor createEmisor(ObjectFactory of) {
         Emisor emisor = of.createComprobanteEmisor();
         emisor.setRfc("LAN7008173R5");
@@ -158,6 +161,7 @@ public class WriteXML {
         return emisor;
     }
     
+    //Genera los datos del receptor
     private Receptor createReceptor(ObjectFactory of) {
         Receptor receptor = of.createComprobanteReceptor();
         receptor.setRfc("SUL010720JN8");
@@ -166,54 +170,92 @@ public class WriteXML {
         return receptor;
     }
     
+    //Crea una la lista de conceptos. Se pueden añadir tantos conceptos como se guste
     private Conceptos createConceptos(ObjectFactory of) {
         Conceptos cps = of.createComprobanteConceptos();
         List<Concepto> list = cps.getConcepto();
-        Concepto c1 = of.createComprobanteConceptosConcepto();
-        c1.setImporte(new BigDecimal("500.00"));
-        c1.setValorUnitario(new BigDecimal("500.00"));
-        c1.setUnidad("Kilogramo");
-        c1.setClaveUnidad("KGM");
-        c1.setCantidad(new BigDecimal("1.00"));
-        c1.setClaveProdServ("50111515");
-        c1.setDescripcion("Pollo en pieza");
-        c1.setNoIdentificacion("12345");
-        c1.setImpuestos(createImpuestosConcepto(of));
-        list.add(c1);
+        
+        //Aquí se pueden agregar los distintos conceptos o productos, junto con su impuesto
+        list.add(createConcepto(of, "500.00", "500.00", "Kilogramo", "KGM", "1.00", "50111515", "Pollo en pieza", "12345",
+                                "80.00", "0.160000", CTipoFactor.TASA, "002", "500.00"));
+        list.add(createConcepto(of, "600.00", "600.00", "Kilogramo", "KGM", "1.00", "50111516", "Res en pieza", "123456",
+                                "96.00", "0.160000", CTipoFactor.TASA, "002", "600.00"));
         return cps;
     }
     
-    private Impuestos createImpuestosConcepto(ObjectFactory of) {
+    //Crea los distintos conceptos. Los parámetros que recibe son tanto del concepto como del impuesto
+    //importeConcepto: cuánto es por la cantidad de productos del mismo tipo
+    //valorUnitario: cuánto cuesta un sólo producto del mismo tipo
+    //unidad: la unidad en que se mide el producto por ejemplo kilogramo
+    //claveUnidad: la clave de esa unidad dada por el sat, para el caso del kilogramo su clave es KGM
+    //cantidad: cantidad de esa unidad de medida, para el caso del kilogramo se definió un kilo (1.00)
+    //claveProdServ: la clave del producto con que viene de la BD
+    //descripcion: difine una pequeña descripción del producto que se está vendiendo
+    //noIdentificacion: es el folio asignado a la operación aplicada a los comprobantes para el público en general
+    //importe: define el iva que se aumentará al producto. En el caso del pollo es 500*0.16 = 90.00
+    //tasaCuota: la tasa del iva, usualmente es 16% que en decimal se define como 0.160000
+    //cTipoFactor: hay 3 tipos TASA,EXCENTO y CUOTA, para cada uno de los casos
+    /*impuesto: El CFDI tiene 4 combinaciones distintas de impuestos de traslado:
+        IVA con tasa del 16%    (Impuesto="002" TipoFactor="Tasa"      TasaOCuota="0.160000")
+        IVA con tasa del 0%      (Impuesto="002" TipoFactor="Exento"    TasaOCuota="0.000000")
+        IEPS con tasa del 25%  (Impuesto="003" TipoFactor="Tasa"      TasaOCuota="0.250000")
+        IEPS con cuota              (Impuesto="003" TipoFactor="Cuota"    TasaOCuota="43.770000")*/
+    //base: básicamente es el precio unitario del producto
+    private Concepto createConcepto(ObjectFactory of, String importeConcepto, String valorUnitario, String unidad, 
+    String claveUnidad, String cantidad, String claveProdServ, String descripcion, String noIdentificacion, String importe, 
+    String tasaCuota, CTipoFactor cTipoFactor, String impuesto, String base) {
+        
+        Concepto concepto = of.createComprobanteConceptosConcepto();
+        concepto.setImporte(new BigDecimal(importeConcepto));
+        concepto.setValorUnitario(new BigDecimal(valorUnitario));
+        concepto.setUnidad(unidad);
+        concepto.setClaveUnidad(claveUnidad);
+        concepto.setCantidad(new BigDecimal(cantidad));
+        concepto.setClaveProdServ(claveProdServ);
+        concepto.setDescripcion(descripcion);
+        concepto.setNoIdentificacion(noIdentificacion);
+        concepto.setImpuestos(createImpuestosConceptoList(of, importe, tasaCuota, cTipoFactor, impuesto, base));
+        return concepto;
+    }
+    
+    //Método que recibe los valores del traslado para cada concepto o producto
+    private Impuestos createImpuestosConceptoList(ObjectFactory of, String importe, 
+    String tasaCuota, CTipoFactor cTipoFactor, String impuesto, String base) {
+        
         Comprobante.Conceptos.Concepto.Impuestos imps = of.createComprobanteConceptosConceptoImpuestos();
         
         //Bloque para los impuestos trasladados.
         Comprobante.Conceptos.Concepto.Impuestos.Traslados trs = of.createComprobanteConceptosConceptoImpuestosTraslados();
         List<Comprobante.Conceptos.Concepto.Impuestos.Traslados.Traslado> list = trs.getTraslado();
-        Comprobante.Conceptos.Concepto.Impuestos.Traslados.Traslado t1 = of.createComprobanteConceptosConceptoImpuestosTrasladosTraslado();
-        t1.setImporte(new BigDecimal("80.00"));
-        t1.setTasaOCuota(new BigDecimal("0.160000"));
-        t1.setTipoFactor(CTipoFactor.TASA);
-        t1.setImpuesto("002");
-        t1.setBase(new BigDecimal("500.00"));
-        list.add(t1);
-         
+        
+        //Se generan los impuestos del concepto
+        Comprobante.Conceptos.Concepto.Impuestos.Traslados.Traslado traslado = of.createComprobanteConceptosConceptoImpuestosTrasladosTraslado();
+        traslado.setImporte(new BigDecimal(importe));
+        traslado.setTasaOCuota(new BigDecimal(tasaCuota));
+        traslado.setTipoFactor(cTipoFactor);
+        traslado.setImpuesto(impuesto);
+        traslado.setBase(new BigDecimal(base));
+        list.add(traslado);
+
         imps.setTraslados(trs);
         return imps;
     }
     
+    //Método para crear los impuestos de la factura
     private Comprobante.Impuestos createImpuestos(ObjectFactory of) {
         Comprobante.Impuestos impus = of.createComprobanteImpuestos();
         
-        impus.setTotalImpuestosTrasladados(new BigDecimal("80.00"));
+        impus.setTotalImpuestosTrasladados(new BigDecimal("176.00"));
         
         // Bloque para los impuestos transladados
         Comprobante.Impuestos.Traslados tras = of.createComprobanteImpuestosTraslados();
         List<Comprobante.Impuestos.Traslados.Traslado>list = tras.getTraslado();
         Comprobante.Impuestos.Traslados.Traslado t1 = of.createComprobanteImpuestosTrasladosTraslado();
-        t1.setImporte(new BigDecimal("80.00"));
+        t1.setImporte(new BigDecimal("176.00"));
         t1.setTasaOCuota(new BigDecimal("0.160000"));
         t1.setTipoFactor(CTipoFactor.TASA);
         t1.setImpuesto("002");
+        
         list.add(t1);
         
         impus.setTraslados(tras);
@@ -238,6 +280,7 @@ public class WriteXML {
         return new String (Base64.encode(cert.getEncoded()));
     }
     
+    //Obtiene el número del certificado
     private String getNoCertificado(final X509Certificate cert) {
         BigInteger serial = cert.getSerialNumber();
         byte[] sArr = serial.toByteArray();
@@ -315,6 +358,47 @@ public class WriteXML {
         return resultado;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
